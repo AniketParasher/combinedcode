acbc9a4b4ef523ca5edec5e9cfaaab01ffed5d46
Aniket Parasher <107147706+AniketParasher@users.noreply.github.com>
2024-09-05 11:18:53 +0530

    Update 4thseptv3.py

--- 4thseptv3.py
+++ 4thseptv3.py
@@ -367,7 +367,7 @@ def main():
     
     # Display the text and table
     st.markdown(css, unsafe_allow_html=True)
-    st.markdown("<p style='font-size: small;'>Please rename your column headers as per input file structure shown:</p>", unsafe_allow_html=True)
+    st.warning("""Please rename your column headers as per input file structure shown""")
     st.markdown(html_table, unsafe_allow_html=True)
     
     st.info(
