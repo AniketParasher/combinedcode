66f1d283430683abdfd125a3f147f57cf34ed30b
Aniket Parasher <107147706+AniketParasher@users.noreply.github.com>
2024-09-18 19:38:27 +0530

    Update 18septv1.py

--- 18septv1.py
+++ 18septv1.py
@@ -451,7 +451,7 @@ def main():
                 """
                 <p style='color: blue;'>
                     Please provide required Digits 
-                    <span style='color: red;'>Selection below minimum required value not allowed</span>
+                    <span style='color: red;'>(Selection below minimum required value not allowed)</span>
                 </p>
                 """, 
                 unsafe_allow_html=True
